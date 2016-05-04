27c80c33400750890afebf1ce5cfc207bfbc8309
EQUIPO <EQUIPO@10.100.239.116>
2016-05-04 16:59:56 +0200

    Subir proyecto.

--- Mindel/src/modelo/database.java
+++ Mindel/src/modelo/database.java
@@ -5,35 +5,47 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class database {
+
+    /**
+     * database
+     */
     private String db = "dam12_Mindel";
-  /** usuario */
-  private String user = "dam12";
-  /** contraseña de MySql*/
-  private String password = "salesianas";
-  /** Cadena de conexion */
-  private String url = "jdbc:mysql://79.148.236.236/"+db;
-  /** variable para trabajar con la conexion a la base de datos */
-  private Connection conn = null;
+    /**
+     * usuario
+     */
+    private String user = "dam12";
+    /**
+     * contraseña de MySql
+     */
+    private String password = "salesianas";
+    /**
+     * Cadena de conexion
+     */
+    private String url = "jdbc:mysql://79.148.236.236/" + db;
+    /**
+     * variable para trabajar con la conexion a la base de datos
+     */
+    private Connection conn = null;
 
-   /** Constructor de clase */
-   public database(){
-        this.url = "jdbc:mysql://79.148.236.236/"+this.db;
-       try{
-         //obtenemos el driver de para mysql
-         Class.forName("com.mysql.jdbc.Driver");
-         //obtenemos la conexión
-         conn = DriverManager.getConnection( this.url, this.user , this.password );    
-         System.out.println("EXITO");
-      }catch(SQLException e){
-         System.err.println( e.getMessage() );
-      }catch(ClassNotFoundException e){
-         System.err.println( e.getMessage() );
-      }
-   }
+    /**
+     * Constructor de clase
+     */
+    public database() {
+        this.url = "jdbc:mysql://79.148.236.236/" + this.db;
+        try {
+            //obtenemos el driver de para mysql
+            Class.forName("com.mysql.jdbc.Driver");
+            //obtenemos la conexión
+            conn = DriverManager.getConnection(this.url, this.user, this.password);
+            System.out.println("EXITO");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 
-
-   public Connection getConexion()
-   {
-    return this.conn;
-   }
+    public Connection getConexion() {
+        return this.conn;
+    }
 }
